9ca609c9ed89343cf48199d58243491ed14f9ed5
Thanos <atha.ar@arcor.de>
2020-06-08 04:07:44 +0200

    boese2

---
+++ boese2/Boese2Logic.py
@@ -0,0 +1,8 @@
+
+class Board():
+    def __init__(self, n):
+        self.n = n
+        # Create the empty board array.
+        self.pieces = [None]*self.n
+        for i in range(self.n):
+            self.pieces[i] = [0]*self.n
